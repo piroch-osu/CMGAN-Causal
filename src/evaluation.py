de401cc75532274b0e8241877cf9fbd9ba08d634
Taylor Hamilton <hamilton1565@ascend-rw01.ten.osc.edu>
2025-11-30 12:13:10 -0500

    Initial timing script for single track enhancement

--- src/evaluation.py
+++ src/evaluation.py
@@ -7,61 +7,79 @@ from utils import *
 import torchaudio
 import soundfile as sf
 import argparse
+import time
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
 
 
 @torch.no_grad()
 def enhance_one_track(
     model, audio_path, saved_dir, cut_len, n_fft=400, hop=100, save_tracks=False
 ):
-    name = os.path.split(audio_path)[-1]
-    noisy, sr = torchaudio.load(audio_path)
-    assert sr == 16000
-    noisy = noisy.cuda()
+    # Start timing
+    start = time.time()
 
+    name = os.path.split(audio_path)[-1]
+    noisy, sr = torchaudio.load(audio_path) # Load audio with shape -> (1, samples)
+    assert sr == 16000
+    noisy = noisy.to(device) # Move to GPU
+
+    # Normalize energy across channels
     c = torch.sqrt(noisy.size(-1) / torch.sum((noisy**2.0), dim=-1))
     noisy = torch.transpose(noisy, 0, 1)
     noisy = torch.transpose(noisy * c, 0, 1)
 
     length = noisy.size(-1)
+    # Pad audio to be a multiple of 100 samples
     frame_num = int(np.ceil(length / 100))
     padded_len = frame_num * 100
     padding_len = padded_len - length
     noisy = torch.cat([noisy, noisy[:, :padding_len]], dim=-1)
+    # Split into chunks if longer than cut_len (16 sec)
     if padded_len > cut_len:
         batch_size = int(np.ceil(padded_len / cut_len))
         while 100 % batch_size != 0:
             batch_size += 1
         noisy = torch.reshape(noisy, (batch_size, -1))
 
+    # Compute STFT
     noisy_spec = torch.stft(
-        noisy, n_fft, hop, window=torch.hamming_window(n_fft).cuda(), onesided=True
+        noisy, n_fft, hop, window=torch.hamming_window(n_fft).to(device), onesided=True
     )
     noisy_spec = power_compress(noisy_spec).permute(0, 1, 3, 2)
+    
+    # Feed through generator network
     est_real, est_imag = model(noisy_spec)
     est_real, est_imag = est_real.permute(0, 1, 3, 2), est_imag.permute(0, 1, 3, 2)
 
     est_spec_uncompress = power_uncompress(est_real, est_imag).squeeze(1)
+    # Inverse STFT
     est_audio = torch.istft(
         est_spec_uncompress,
         n_fft,
         hop,
-        window=torch.hamming_window(n_fft).cuda(),
+        window=torch.hamming_window(n_fft).to(device),
         onesided=True,
     )
-    est_audio = est_audio / c
-    est_audio = torch.flatten(est_audio)[:length].cpu().numpy()
+    est_audio = est_audio / c # Undo earlier normalization
+    est_audio = torch.flatten(est_audio)[:length].cpu().numpy() # Remove padding
     assert len(est_audio) == length
     if save_tracks:
         saved_path = os.path.join(saved_dir, name)
         sf.write(saved_path, est_audio, sr)
 
-    return est_audio, length
+    # End timing
+    end = time.time()
+    runtime = end - start
+
+    return est_audio, length, runtime
 
 
 def evaluation(model_path, noisy_dir, clean_dir, save_tracks, saved_dir):
     n_fft = 400
-    model = generator.TSCNet(num_channel=64, num_features=n_fft // 2 + 1).cuda()
-    model.load_state_dict((torch.load(model_path)))
+    model = generator.TSCNet(num_channel=64, num_features=n_fft // 2 + 1).to(device)
+    model.load_state_dict((torch.load(model_path, map_location=device)))
     model.eval()
 
     if not os.path.exists(saved_dir):
@@ -103,7 +121,7 @@ def evaluation(model_path, noisy_dir, clean_dir, save_tracks, saved_dir):
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_path", type=str, default='./best_ckpt/ckpt_80',
                     help="the path where the model is saved")
-parser.add_argument("--test_dir", type=str, default='dir to your VCTK-DEMAND test dataset',
+parser.add_argument("--test_dir", type=str, default='/users/PAS3239/hamilton1565/CMGAN-Causal/VCTK-DEMAND',
                     help="noisy tracks dir to be enhanced")
 parser.add_argument("--save_tracks", type=str, default=True, help="save predicted tracks or not")
 parser.add_argument("--save_dir", type=str, default='./saved_tracks_best', help="where enhanced tracks to be saved")
